bfb6c8e66d6066bd362076bc78232b42a30c838c
Мак <mak@MacBook-Air-Mak.local>
2022-11-30 22:57:07 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/personal-portfolio/precache-manifest.28673a0905cea5b0668a9a1ba3c083d9.js"
+  "/personal-portfolio/precache-manifest.769a02e19859969f954909026bf5efe9.js"
 );
 
 workbox.clientsClaim();
